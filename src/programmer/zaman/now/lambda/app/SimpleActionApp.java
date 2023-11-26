88caffbaba1657b27b44d304a5f7fa2699963e7a
adenafil <nafilie9@gmail.com>
2023-11-26 10:17:47 +0700

    Membuat lambda

--- src/programmer/zaman/now/lambda/app/SimpleActionApp.java
+++ src/programmer/zaman/now/lambda/app/SimpleActionApp.java
@@ -5,20 +5,40 @@ import programmer.zaman.now.lambda.SimpleAction;
 public class SimpleActionApp {
     public static void main(String[] args) {
 
+/*
         SimpleAction simpleAction1 = new SimpleAction() {
             @Override
-            public String action() {
+            public String action(String name) {
                 return "Ade";
             }
         };
 
-        System.out.println(simpleAction1.action());
+        System.out.println(simpleAction1.action("Ade"));
 
-        SimpleAction simpleAction2 = () -> {
+        SimpleAction simpleAction2 = (String name) -> {
             return "Ade Lambda";
         };
 
-        System.out.println(simpleAction2.action());
+        System.out.println(simpleAction2.action("Ade Lambda"));
+*/
+
+/*
+        SimpleAction simpleAction1 = (String name) -> {
+            return "Hello " + name;
+        };
+
+        SimpleAction simpleAction2 = (name) -> {
+            return "Hello " + name;
+        };
+
+        SimpleAction simpleAction3 = name -> "Hello" + name;
+*/
+
+        SimpleAction simpleAction1 = (String name) -> "Hello " + name;
+
+        SimpleAction simpleAction2 = (name) -> "Hello " + name;
+
+        SimpleAction simpleAction3 = name -> "Hello " + name;
 
     }
 }
